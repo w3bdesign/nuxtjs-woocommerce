724a8fc550050fcee1ff2141744a839562055cf5
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-11-09 06:15:24 +0100

    Add a proper footer

--- components/Footer/Footer.vue
+++ components/Footer/Footer.vue
@@ -1,12 +1,21 @@
 <template>
   <div class="container mx-auto">
-    <footer class="mt-6 text-lg text-center">Copyright (c) 2020</footer>
+    <footer
+      class="px-6 text-center bg-white border border-gray-300 rounded-lg shadow-lg"
+    >
+      <div class="p-6">Copyright &copy; {{ todayDate }} Daniel / w3bdesign</div>
+    </footer>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Footer',
+  computed: {
+    todayDate() {
+      return new Date().getFullYear()
+    },
+  },
 }
 </script>
 

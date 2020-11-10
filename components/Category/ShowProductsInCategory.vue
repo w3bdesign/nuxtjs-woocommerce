4d68063eeabc0f1b43924ddcbf14a334ec814c50
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-11-10 05:10:54 +0100

    Show message if no products are in a category

--- components/Category/ShowProductsInCategory.vue
+++ components/Category/ShowProductsInCategory.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="container mx-auto">
     <div v-if="products">
-      <div>
-        <h1 class="h-10 text-3xl font-bold text-center">{{ products.name }}</h1>
-        <br />
-        <ShowAllProducts :products="products.products.nodes" />
+      <h1 class="h-10 p-6 text-3xl font-bold text-center">
+        {{ products.name }}
+      </h1>
+      <br />
+      <ShowAllProducts :products="products.products.nodes" />
+      <div v-if="noProducts" class="mt-10">
+        <h2 class="h-10 text-2xl font-bold text-center">
+          No products to display
+        </h2>
       </div>
     </div>
     <div v-else><LoadingSpinner /></div>
@@ -15,6 +20,11 @@
 export default {
   props: {
     products: { type: Object, required: true },
+  },
+  computed: {
+    noProducts() {
+      return this.products.products.nodes.length === 0
+    },
   },
 }
 </script>

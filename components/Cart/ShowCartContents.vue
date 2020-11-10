e4e63ad3ab4ac4ce26d45226d22697684ee76e2a
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-11-10 06:07:08 +0100

    Show cart contents on cart page

--- components/Cart/ShowCartContents.vue
+++ components/Cart/ShowCartContents.vue
@@ -1,14 +1,24 @@
 <template>
-  <div class="container mx-auto">
+  <div>
     <h1 class="h-10 p-6 text-3xl font-bold text-center">Cart</h1>
-    <pre>
-          {{ this.$store.state.cart }}
-
-      </pre
-    >
+    <div class="container flex flex-wrap items-center mx-auto">
+      <pre>{{ cartContents }}</pre>
+      <br />
+      <button
+        class="w-48 h-12 px-4 py-2 mx-auto mt-12 font-bold text-white bg-blue-500 rounded hover:bg-blue-800"
+      >
+        CHECKOUT
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  computed: {
+    cartContents() {
+      return this.$store.state.cart
+    },
+  },
+}
 </script>

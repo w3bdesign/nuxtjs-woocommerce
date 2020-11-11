282498f432acb274bda6d4c84954988b5d709672
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-11-11 06:12:45 +0100

    We can now fetch the cart, but we need to store it in our VueX store

--- plugins/apollo-client-config.js
+++ plugins/apollo-client-config.js
@@ -10,8 +10,15 @@ const fragmentMatcher = new IntrospectionFragmentMatcher({
 })
 
 export default function (_context) {
+  // https://github.com/nuxt-community/apollo-module/issues/176
   return {
     httpEndpoint: process.env.graphqlUrl,
+    httpLinkOptions: {
+      headers: {
+        'woocommerce-session':
+          'Session eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvd29vLmRmd2ViLm5vIiwiaWF0IjoxNjA1MDcwNDc2LCJuYmYiOjE2MDUwNzA0NzYsImV4cCI6MTYwNTI0MzI3NiwiZGF0YSI6eyJjdXN0b21lcl9pZCI6ImMzNTYwYzU1MmZlOWU0NzRkODE3MTFjNmY4OTVhYjAwIn19.6NeoHz0XF2-TDvABV0hMgx1MJwMFalw2KdTRvkKWWnc',
+      },
+    },
     cache: new InMemoryCache({ fragmentMatcher }),
   }
 }

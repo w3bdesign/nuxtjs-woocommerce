6f4a0dfd45e387a075f47cf6827f29a670bc7af4
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-11-12 04:26:27 +0100

    We no longer need a separate button for adding to the remote cart

--- components/Cart/AddToCartButton.vue
+++ components/Cart/AddToCartButton.vue
@@ -8,12 +8,6 @@
     </button>
     <button
       class="w-48 h-12 px-4 py-2 mt-4 font-bold text-white bg-blue-500 rounded hover:bg-blue-800"
-      @click="addProductToWooCart(product)"
-    >
-      ADD TO WOO CART
-    </button>
-    <button
-      class="w-48 h-12 px-4 py-2 mt-4 font-bold text-white bg-blue-500 rounded hover:bg-blue-800"
       @click="getWooCart"
     >
       FETCH WOO CART

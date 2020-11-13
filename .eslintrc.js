3e10640257f64b3a97a0f45f646e2d78b71a13da
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-11-13 02:20:16 +0100

    Fix CR error

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,5 +16,12 @@ module.exports = {
   ],
   plugins: ['prettier'],
   // add your custom rules here
-  rules: {},
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
 }

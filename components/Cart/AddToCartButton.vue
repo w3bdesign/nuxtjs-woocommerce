0f57b0fd727eb4797a435f56e286a6d9039781ea
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-11-17 04:35:32 +0100

    Remove temporary fetch Woo cart button

--- components/Cart/AddToCartButton.vue
+++ components/Cart/AddToCartButton.vue
@@ -6,12 +6,6 @@
     >
       ADD TO CART
     </button>
-    <button
-      class="w-48 h-12 px-4 py-2 mt-4 font-bold text-white bg-blue-500 rounded hover:bg-blue-800"
-      @click="getWooCart"
-    >
-      FETCH WOO CART
-    </button>
   </div>
 </template>
 

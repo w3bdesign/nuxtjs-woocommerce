fe89d71ce9ff87a95edc0c5c728dfa50d57ed2c4
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-11-22 03:16:08 +0100

    Add all necessary checkout fields

--- components/Checkout/CheckoutForm.vue
+++ components/Checkout/CheckoutForm.vue
@@ -1,11 +1,76 @@
 <template>
-  <div class="container mx-auto form-wrapper">
+  <div class="container mx-auto">
     <FormulateForm v-model="formData">
-      <FormulateInput
-        class="p-2 text-gray-800 placeholder-gray-800 border border-gray-400 rounded shadow-md"
-        label="Who is going to attend?"
-        type="text"
-      />
+      <section className="text-gray-700">
+        <div className="container p-4 py-2 mx-auto">
+          <div className="mx-auto lg:w-1/2 md:w-2/3">
+            <div className="w-1/2 p-2">
+              <FormulateInput
+                name="firstName"
+                class="p-2 text-gray-800 placeholder-gray-800 border border-gray-400 rounded shadow-md"
+                label="First name"
+                type="text"
+              />
+            </div>
+            <div className="w-1/2 p-2">
+              <FormulateInput
+                name="lastName"
+                class="p-2 text-gray-800 placeholder-gray-800 border border-gray-400 rounded shadow-md"
+                label="Last name"
+                type="text"
+              />
+            </div>
+            <div className="w-1/2 p-2">
+              <FormulateInput
+                name="address1"
+                class="p-2 text-gray-800 placeholder-gray-800 border border-gray-400 rounded shadow-md"
+                label="Address"
+                type="text"
+              />
+            </div>
+            <div className="w-1/2 p-2">
+              <FormulateInput
+                name="postcode"
+                class="p-2 text-gray-800 placeholder-gray-800 border border-gray-400 rounded shadow-md"
+                label="Postal code"
+                type="text"
+              />
+            </div>
+            <div className="w-1/2 p-2">
+              <FormulateInput
+                name="city"
+                class="p-2 text-gray-800 placeholder-gray-800 border border-gray-400 rounded shadow-md"
+                label="City"
+                type="text"
+              />
+            </div>
+            <div className="w-1/2 p-2">
+              <FormulateInput
+                name="email"
+                class="p-2 text-gray-800 placeholder-gray-800 border border-gray-400 rounded shadow-md"
+                label="Email"
+                type="text"
+              />
+            </div>
+            <div className="w-1/2 p-2">
+              <FormulateInput
+                name="phone"
+                class="p-2 text-gray-800 placeholder-gray-800 border border-gray-400 rounded shadow-md"
+                label="Phone"
+                type="text"
+              />
+            </div>
+            <div className="w-1/2 p-2">
+              <FormulateInput
+                name="paymentMethod"
+                class="p-2 text-gray-800 placeholder-gray-800 border border-gray-400 rounded shadow-md"
+                value="cod"
+                type="hidden"
+              />
+            </div>
+          </div>
+        </div>
+      </section>
     </FormulateForm>
   </div>
 </template>

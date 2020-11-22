0116c25635b16bd2b6b4599e4a42d8352bd9286e
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-11-22 02:13:11 +0100

    Update packages and add sample products

---
+++ components/Checkout/CheckoutForm.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container mx-auto form-wrapper">
+    <FormulateForm v-model="formData">
+      <FormulateInput
+        class="p-2 text-gray-800 placeholder-gray-800 border border-gray-400 rounded shadow-md"
+        label="Who is going to attend?"
+        type="text"
+      />
+    </FormulateForm>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CheckoutForm',
+  data() {
+    return {
+      formData: {},
+    }
+  },
+}
+</script>

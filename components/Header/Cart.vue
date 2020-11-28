ba9ab87c2188434f1351a187bb4f125fc9020b66
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-11-28 23:36:43 +0100

    Show an error if we can't get the cart

--- components/Header/Cart.vue
+++ components/Header/Cart.vue
@@ -1,34 +1,38 @@
 <template>
   <div>
-    <NuxtLink to="/cart">
-      <transition name="cart">
-        <span
-          v-if="cartLength"
-          class="text-xl text-white no-underline lg:text-black is-active"
-        >
-          <img
-            alt="Cart icon"
-            class="h-12"
-            aria-label="Cart"
-            src="~/assets/Cart.svg"
-        /></span>
-      </transition>
-      <transition name="cart">
-        <div v-if="cartLength">
+    <span v-if="remoteError" class="text-xl text-red-500"
+      >Error getting cart</span
+    >
+    <div v-else>
+      <NuxtLink to="/cart">
+        <transition name="cart">
           <span
-            class="absolute w-6 h-6 pb-2 ml-12 -mt-12 text-center text-white bg-black rounded-full"
+            v-if="cartLength"
+            class="text-xl text-white no-underline lg:text-black is-active"
           >
-            {{ cartLength }}
-          </span>
-          <span>Total: {{ subTotal }}</span>
-        </div>
-      </transition>
-    </NuxtLink>
+            <img
+              alt="Cart icon"
+              class="h-12"
+              aria-label="Cart"
+              src="~/assets/Cart.svg"
+          /></span>
+        </transition>
+        <transition name="cart">
+          <div v-if="cartLength">
+            <span
+              class="absolute w-6 h-6 pb-2 ml-12 -mt-12 text-center text-white bg-black rounded-full"
+            >
+              {{ cartLength }}
+            </span>
+            <span>Total: {{ subTotal }}</span>
+          </div>
+        </transition>
+      </NuxtLink>
+    </div>
   </div>
 </template>
 
 <script>
-// import useFetchWooCart from '@/hooks/useFetchWooCart'
 import GET_CART_QUERY from '@/apollo/queries/GET_CART_QUERY'
 
 export default {
@@ -49,35 +53,18 @@ export default {
       result({ data, loading, networkStatus }) {
         const cartIsReady = networkStatus === 7
         if (cartIsReady) {
-          console.log('Cart is ready')
           this.remoteCart = data
           this.subTotal = data.cart.total
           this.cartLength = data.cart.contents.nodes.reduce(
             (accumulator, argument) => accumulator + argument.quantity,
             0
           )
-          // return { remoteCart, cartLength, subTotal }
         }
+      },
+      error(error) {
+        this.remoteError = error
       },
     },
   },
-
-  /* async mounted() {
-    const {
-      remoteCart,
-      cartLength,
-      subTotal,
-      remoteError,
-    } = await useFetchWooCart(this)
-
-    if (remoteCart) {
-      this.remoteCart = remoteCart
-      this.cartLength = cartLength
-      this.subTotal = subTotal
-    }
-    if (remoteError) {
-      this.remoteError = remoteError
-    }
-  }, */
 }
 </script>

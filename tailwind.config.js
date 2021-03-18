abf94a10950fbd66514083da47dd17b3b24e8a40
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2021-03-18 01:18:20 +0100

    Upgrade Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,24 +2,6 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    zIndex: {
-      0: 0,
-
-      10: 10,
-
-      20: 20,
-
-      30: 30,
-
-      40: 40,
-
-      50: 50,
-
-      75: 75,
-
-      100: 100,
-      auto: 'auto',
-    },
     extend: {},
   },
   variants: {

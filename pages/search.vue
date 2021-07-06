61960202030cea1391d1523090f24d87f6ecf3c2
Daniel Fjeldstad <daniel.fjeldstad@novacare.no>
2021-07-06 02:45:31 +0200

    Upgrade Algolia theme version

--- pages/search.vue
+++ pages/search.vue
@@ -27,6 +27,7 @@
         </p>
       </template>
     </ais-hits>
+    <br />
     <ais-pagination />
   </ais-instant-search-ssr>
 </template>
@@ -70,7 +71,7 @@ export default {
       link: [
         {
           rel: 'stylesheet',
-          href: 'https://cdn.jsdelivr.net/npm/instantsearch.css@7.3.1/themes/algolia-min.css',
+          href: 'https://cdn.jsdelivr.net/npm/instantsearch.css@7.4.5/themes/algolia-min.css',
         },
       ],
     }
@@ -92,3 +93,15 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+li {
+  width: 100vw !important;
+}
+
+@media (max-width: 768px) {
+  .ais-Hits-item {
+    width: 100vw !important;
+  }
+}
+</style>

8f53083abef6d1820b8de44f199eb930babccba5
Daniel Fjeldstad <daniel.fjeldstad@novacare.no>
2021-11-19 05:49:41 +0100

    Remove Vue Awesome Swiper

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,10 +33,7 @@ export default {
   css: ['@/assets/css/animate.min.css'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
-    '~/plugins/vue-formulate',
-    { src: '~/plugins/vue-awesome-swiper', mode: 'client', ssr: false },
-  ],
+  plugins: ['~/plugins/vue-formulate'],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,

8c5501e4eef0968e7ae52352ce34c831099f31f9
Daniel Fjeldstad <daniel.fjeldstad@novacare.no>
2021-11-19 05:30:13 +0100

    Remove outdated Swiper

--- components/Index/Hero.vue
+++ components/Index/Hero.vue
@@ -1,103 +1,37 @@
 <template>
-  <div>
-    <swiper id="hero" ref="mySwiper" :options="swiperOption">
-      <swiper-slide
-        ><img alt="Image slider slide #1" src="~assets/images/Hero.jpg" />
-        <div
-          class="
-            flex flex-col
-            items-start
-            justify-center
-            w-full
-            mx-auto
-            tracking-wide
-            lg:w-1/2
-          "
-        >
-          <span
-            class="
-              w-full
-              p-4
-              mt-4
-              text-center text-white
-              bg-black
-              text-md
-              lg:text-2xl lg:-mt-64
-            "
-          >
-            Modern Pillow Sample Set
-          </span>
-        </div>
-      </swiper-slide>
-      <swiper-slide
-        ><img alt="Image slider slide #2" src="~assets/images/Hero2.jpg" />
-        <div
-          class="
-            flex flex-col
-            items-start
-            justify-center
-            w-full
-            mx-auto
-            tracking-wide
-            lg:w-1/2
-          "
-        >
-          <span
-            class="
-              w-full
-              p-4
-              mt-4
-              text-center text-white
-              bg-black
-              text-md
-              lg:text-2xl lg:-mt-64
-            "
-          >
-            Modern Interior Sample
-          </span>
-        </div>
-      </swiper-slide>
-      <div
-        slot="button-prev"
-        class="swiper-button-prev"
-        @click="swiper.slidePrev(1000, false)"
-      ></div>
-      <div
-        slot="button-next"
-        class="swiper-button-next"
-        @click="swiper.slideNext(1000, false)"
-      ></div>
-    </swiper>
+  <div id="hero">
+    <img alt="Image slider slide #1" src="~assets/images/Hero.jpg" />
+    <div
+      class="
+        flex flex-col
+        items-start
+        justify-center
+        w-full
+        mx-auto
+        tracking-wide
+        lg:w-1/2
+      "
+    >
+      <span
+        class="
+          w-full
+          p-4
+          mt-4
+          text-center text-white
+          bg-black
+          text-md
+          lg:text-2xl lg:-mt-64
+        "
+      >
+        Modern Interior Sample
+      </span>
+    </div>
   </div>
 </template>
 
 <script>
-import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
-
 export default {
   name: 'Hero',
-  components: {
-    Swiper,
-    SwiperSlide,
-  },
-  data() {
-    return {
-      swiperOption: {
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-      },
-    }
-  },
-  computed: {
-    swiper() {
-      return this.$refs.mySwiper.$swiper
-    },
-  },
-  mounted() {
-    this.swiper.slideTo(2, 2000, false)
-  },
 }
 </script>
 

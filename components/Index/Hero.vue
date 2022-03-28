c3ff2bdff5ea306c63d77e766452f5cddb824cb9
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-03-28 20:16:52 +0200

    Fiks hero bilde

--- components/Index/Hero.vue
+++ components/Index/Hero.vue
@@ -1,6 +1,6 @@
 <template>
-  <div id="hero">
-    <img alt="Image slider slide #1" src="~assets/images/Hero.jpg" />
+  <div>
+    <img class="mx-auto" alt="Image slider slide #1" src="~assets/images/Hero.jpg" />
     <div
       class="flex flex-col items-start justify-center w-full mx-auto tracking-wide lg:w-1/2"
     >

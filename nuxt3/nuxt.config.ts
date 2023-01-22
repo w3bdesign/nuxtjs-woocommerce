146fcfaf57687b9f764ebddd58a2e7f214f480e9
codefactor-io <support@codefactor.io>
2023-01-22 19:30:26 +0000

    [CodeFactor] Apply fixes

--- nuxt3/nuxt.config.ts
+++ nuxt3/nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
-import { defineNuxtConfig } from "nuxt/config";
+import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
   modules: ['@pinia/nuxt'],

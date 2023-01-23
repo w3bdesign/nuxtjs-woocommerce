bb13df1c6667a4424b4f13a4842b3ec978a8d34e
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-01-23 04:13:57 +0100

    Add title

--- nuxt3/app.vue
+++ nuxt3/app.vue
@@ -1,5 +1,7 @@
 <template>
-  <Html>
+  <!DOCTYPE html>
+  <Html lang="en">
+  <title>Nuxt 3 - Woocommerce</title>
     <Body
       class="antialiased duration-300 transition-colors text-gray-800 dark:text-gray-200 bg-white dark:bg-gray-900"
     >

ea7197ae8d59fd365ec466a90d8958317b60614f
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-01-24 00:02:27 +0100

    Categories page

---
+++ nuxt3/pages/categories.vue
@@ -0,0 +1,15 @@
+<template>
+  <div v-if="data.productCategories">
+    <CategoryShowAll :categories="data.productCategories" />
+  </div>
+  <div v-else>
+    <SpinnerLoading />
+  </div>
+</template>
+
+<script setup>
+import FETCH_ALL_CATEGORIES_QUERY from '@/apollo/queries/FETCH_ALL_CATEGORIES_QUERY.gql'
+
+const variables = { limit: 5 }
+const { data } = await useAsyncQuery(FETCH_ALL_CATEGORIES_QUERY, variables)
+</script>

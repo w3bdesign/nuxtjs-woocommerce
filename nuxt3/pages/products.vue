a753955f3fca939c0a53c17abd914a7ab3288486
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-01-23 06:17:10 +0100

    Show all products page

---
+++ nuxt3/pages/products.vue
@@ -0,0 +1,12 @@
+<template>
+  <div v-if="data.products">
+    <ProductsShowAll :products="data.products.nodes" />
+  </div>
+</template>
+
+<script setup>
+import FETCH_ALL_PRODUCTS_QUERY from '@/apollo/queries/FETCH_ALL_PRODUCTS_QUERY.gql'
+
+const variables = { limit: 5 }
+const { data } = await useAsyncQuery(FETCH_ALL_PRODUCTS_QUERY, variables)
+</script>

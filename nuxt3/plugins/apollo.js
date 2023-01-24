ebf6e4f53acf71b457afda7b02393ef7d35576ee
codefactor-io <support@codefactor.io>
2023-01-24 04:58:30 +0000

    [CodeFactor] Apply fixes

--- nuxt3/plugins/apollo.js
+++ nuxt3/plugins/apollo.js
@@ -6,13 +6,10 @@ export default defineNuxtPlugin((nuxtApp) => {
   const headers = useRequestHeaders(['woocommerce-session'])
   nuxtApp.hook('apollo:auth', ({ client, token }) => {
     // `client` can be used to differentiate logic on a per-client basis.
-
-    //console.log('Token: ', token)
-    //console.log('My Token: ', mytoken)
+    // console.log('Token: ', token)
+    // console.log('My Token: ', mytoken)
     // console.log('wooToken: ', wooToken)
-
-    //console.log('woo headers: ', headers)
-
+    // console.log('woo headers: ', headers)
     // apply apollo client token
     // token.value = githubToken;
   })

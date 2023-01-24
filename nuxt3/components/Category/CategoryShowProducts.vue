4d5e66da00351e450594dd11e3f6f4d44de31f4a
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-01-24 01:14:32 +0100

    Show products in category

---
+++ nuxt3/components/Category/CategoryShowProducts.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container mx-auto">
+    <div v-if="props.products">
+      <h1 class="h-10 p-6 text-3xl font-bold text-center">
+        {{ props.products.name }}
+      </h1>
+      <br />
+      <ProductsShowAll :products="props.products.products.nodes" />
+      <div v-if="noProducts" class="mt-10">
+        <h2 class="h-10 text-2xl font-bold text-center">
+          No products to display
+        </h2>
+      </div>
+    </div>
+    <div v-else><SpinnerLoading /></div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  products: {
+    type: [Object],
+  },
+})
+
+const noProducts = ref(false)
+
+/*const noProducts = computed(() => {
+  return products.products.nodes.length === 0
+})*/
+</script>
---
+++ nuxt3/pages/category/[category].vue
@@ -0,0 +1,20 @@
+<template>
+  <div v-if="data?.productCategory">
+    <CategoryShowProducts :products="data.productCategory" />
+  </div>
+  <div v-else>
+    <SpinnerLoading />
+  </div>
+</template>
+
+<script setup>
+import GET_PRODUCTS_FROM_CATEGORY_QUERY from '@/apollo/queries/GET_PRODUCTS_FROM_CATEGORY_QUERY.gql'
+
+const route = useRoute()
+
+const variables = { id: route.query.id, slug: route.params.category }
+const { data } = await useAsyncQuery(
+  GET_PRODUCTS_FROM_CATEGORY_QUERY,
+  variables
+)
+</script>

ce2efd38f4e0203bf38ed20a7498454691f5f0b2
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-01-24 05:50:59 +0100

    Remove console log

--- nuxt3/plugins/apollo.js
+++ nuxt3/plugins/apollo.js
@@ -7,11 +7,11 @@ export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.hook('apollo:auth', ({ client, token }) => {
     // `client` can be used to differentiate logic on a per-client basis.
 
-    console.log('Token: ', token)
-    console.log('My Token: ', mytoken)
+    //console.log('Token: ', token)
+    //console.log('My Token: ', mytoken)
     // console.log('wooToken: ', wooToken)
 
-    console.log('woo headers: ', headers)
+    //console.log('woo headers: ', headers)
 
     // apply apollo client token
     // token.value = githubToken;

1a99c2779723c0855fbfbae0e1dc22fb7bc79138
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-01-24 05:54:45 +0100

    Update app.vue

--- nuxt3/app.vue
+++ nuxt3/app.vue
@@ -11,3 +11,19 @@
     </Body>
   </Html>
 </template>
+
+<script setup>
+useHead({
+  title: 'Index',
+  titleTemplate: '%s - Nuxt 3 Woocommerce',
+  meta: [
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    {
+      hid: 'description',
+      name: 'description',
+      content: 'Nuxt 3 Woocommerce',
+    },
+  ],
+  link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+})
+</script>

be34a9bee99b6622d3c1ead137defb0e872ea0cf
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-01-24 01:30:33 +0100

    Show single product

---
+++ nuxt3/components/Products/ProductsSingleProduct.vue
@@ -0,0 +1,87 @@
+<template>
+  <div v-if="props.product">
+    <section>
+      <div class="container flex flex-wrap items-center pt-4 pb-12 mx-auto">
+        <div
+          class="grid grid-cols-1 gap-4 mt-8 lg:grid-cols-2 xl:grid-cols-2 md:grid-cols-2 sm:grid-cols-2"
+        >
+          <img
+            v-if="props.product.image !== undefined"
+            id="product-image"
+            class="h-auto p-8 transition duration-500 ease-in-out transform xl:p-2 md:p-2 lg:p-2 hover:grow hover:shadow-lg hover:scale-105"
+            :alt="props.product.name"
+            :src="props.product.image.sourceUrl"
+          />
+          <img
+            v-else
+            id="product-image"
+            class="h-auto p-8 transition duration-500 ease-in-out transform xl:p-2 md:p-2 lg:p-2 hover:grow hover:shadow-lg hover:scale-105"
+            :alt="props.product.name"
+            :src="process.env.placeholderSmallImage"
+          />
+          <div class="ml-8">
+            <p class="text-3xl font-bold text-left">{{ props.product.name }}</p>
+            <div v-if="props.product.onSale" class="flex">
+              <p class="pt-1 mt-4 text-3xl text-gray-900">
+                <span v-if="product.variations">
+                  {{ filteredVariantPrice(props.product.price) }}</span
+                >
+                <span v-else>{{ props.product.salePrice }}</span>
+              </p>
+              <p class="pt-1 pl-8 mt-4 text-2xl text-gray-900 line-through">
+                <span v-if="props.product.variations">
+                  {{ filteredVariantPrice(props.product.price, 'right') }}</span
+                >
+                <span v-else>{{ props.product.regularPrice }}</span>
+              </p>
+            </div>
+            <p v-else class="pt-1 mt-4 text-2xl text-gray-900">
+              {{ props.product.price }}
+            </p>
+            <br />
+            <p class="pt-1 mt-4 text-2xl text-gray-900">
+              {{ stripHTML(props.product.description) }}
+            </p>
+            <p
+              v-if="props.product.variations"
+              class="pt-1 mt-4 text-xl text-gray-900"
+            >
+              <span class="py-2">Varianter</span>
+              <select
+                id="variant"
+                name="variant"
+                class="block w-64 px-6 py-2 bg-white border border-gray-500 rounded-lg focus:outline-none focus:shadow-outline"
+              >
+                <option
+                  v-for="(variation, index) in props.product.variations.nodes"
+                  :key="variation.databaseId"
+                  :value="variation.databaseId"
+                  :selected="index === 0"
+                >
+                  {{ variation.name }}
+                </option>
+              </select>
+            </p>
+            <div class="pt-1 mt-2">
+              <CartAddToCartButton
+                v-if="props.product.variations"
+                :product="18"
+              />
+              <CartAddToCartButton v-else :product="product" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script setup>
+import { stripHTML, filteredVariantPrice } from '@/utils/functions'
+
+const props = defineProps({
+  product: {
+    type: [Object],
+  },
+})
+</script>
---
+++ nuxt3/pages/product/[product].vue
@@ -0,0 +1,17 @@
+<template>
+  <div v-if="data?.product">
+    <ProductsSingleProduct :product="data.product" />
+  </div>
+  <div v-else>
+    <SpinnerLoading />
+  </div>
+</template>
+
+<script setup>
+import GET_SINGLE_PRODUCT_QUERY from '@/apollo/queries/GET_SINGLE_PRODUCT_QUERY.gql'
+
+const route = useRoute()
+
+const variables = { id: route.query.id, slug: route.params.product }
+const { data } = await useAsyncQuery(GET_SINGLE_PRODUCT_QUERY, variables)
+</script>

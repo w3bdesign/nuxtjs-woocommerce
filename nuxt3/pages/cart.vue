fee983baae98ef9ae55ec714c28d4f4cb7b45dd9
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-01-25 03:24:14 +0100

    Page title

--- nuxt3/pages/cart.vue
+++ nuxt3/pages/cart.vue
@@ -3,3 +3,19 @@
     <CartContents display-remove />
   </div>
 </template>
+
+<script setup>
+useHead({
+  title: 'Cart',
+  titleTemplate: '%s - Nuxt 3 Woocommerce',
+  meta: [
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    {
+      hid: 'description',
+      name: 'description',
+      content: 'Nuxt 3 Woocommerce',
+    },
+  ],
+  link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+})
+</script>

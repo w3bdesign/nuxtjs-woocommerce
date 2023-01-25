0ddac0255e6ca14d5ecae8887bccb9f06b644670
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-01-25 02:22:26 +0100

    Start work on checkout

---
+++ nuxt3/pages/checkout.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <CartShowCartContents />
+    <OrderCheckoutForm />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Checkout',
+  layout: 'Layout',
+  transition: {
+    name: 'home',
+    mode: 'out-in',
+  },
+}
+</script>

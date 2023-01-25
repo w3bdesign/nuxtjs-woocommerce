69dcc5809dc6be30d7f9891377dbc0a254912a3d
codefactor-io <support@codefactor.io>
2023-01-25 02:22:08 +0000

    [CodeFactor] Apply fixes

--- nuxt3/components/Checkout/CheckoutForm.vue
+++ nuxt3/components/Checkout/CheckoutForm.vue
@@ -8,7 +8,7 @@
           class="w-1/2 p-2"
         >
           <label :for="field.inputId">{{ field.label }}</label>
-          <Field :name="field.inputId" v-slot="{ field, meta }">
+          <Field v-slot="{ field, meta }" :name="field.inputId">
             <input
               v-bind="field"
               class="w-full px-4 py-2 mt-2 text-base bg-white border border-gray-400 rounded focus:outline-none focus:border-black"
@@ -19,7 +19,7 @@
               ]"
             />
           </Field>
-          <ErrorMessage :name="field.inputId" v-slot="{ message }">
+          <ErrorMessage v-slot="{ message }" :name="field.inputId">
             <span class="text-lg text-red-500 font-bold">{{
               upperCaseFirstChar(message)
             }}</span>
@@ -38,7 +38,7 @@ import { Form, Field, ErrorMessage } from 'vee-validate'
 
 import { BILLING_FIELDS, BILLING_SCHEMA } from './constants/BILLING_FIELDS'
 
-//import { checkoutOrder } from '@/graphql/mutations/checkoutOrder'
+// import { checkoutOrder } from '@/graphql/mutations/checkoutOrder'
 
 const upperCaseFirstChar = (input) =>
   input.charAt(0).toUpperCase() + input.slice(1)
@@ -61,17 +61,17 @@ const handleSubmit = (values) => {
   }
 
   const checkoutData = {
-    //clientMutationId: uid(),
+    // clientMutationId: uid(),
     clientMutationId: '12345678abcdef',
-    billing: billing,
+    billing,
     shipping: billing,
     shipToDifferentAddress: false,
-    paymentMethod: paymentMethod,
+    paymentMethod,
     isPaid: false,
     transactionId: 'hjkhjkhsdsdiui',
   }
 
-  /*try {
+  /* try {
     checkoutOrder(checkoutData).then((result) => {
       if (result === 'success') {
         location.href = '/success'
@@ -83,6 +83,6 @@ const handleSubmit = (values) => {
     if (import.meta.env.DEV) {
       console.log('Error: ', e)
     }
-  }*/
+  } */
 }
 </script>

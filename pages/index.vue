f4bf8923fbe41fbea639f1bb396164736fa5562f
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-01-28 03:14:01 +0100

    Test deploy

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,5 @@
 <template>
   <div>
     <IndexHero />
-    <div v-if="data.products">
-      <ProductsShowAll :products="data.products.nodes" />
-    </div>
   </div>
 </template>
-
-<script setup>
-import FETCH_ALL_PRODUCTS_QUERY from "@/apollo/queries/FETCH_ALL_PRODUCTS_QUERY.gql";
-
-const variables = { limit: 5 };
-const { data } = await useAsyncQuery(FETCH_ALL_PRODUCTS_QUERY, variables);
-</script>

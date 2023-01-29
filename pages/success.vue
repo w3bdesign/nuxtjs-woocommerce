7c68a45cd506ab23a53aa1e4131d7f1f0937baaf
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-01-29 04:51:36 +0100

    Success page

---
+++ pages/success.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Thank you for your order!</h1>
+    <h2>Please check your email for an order confirmation</h2>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  @apply h-10 p-6 text-3xl font-bold text-center;
+}
+
+h2 {
+  @apply h-10 p-6 text-2xl font-bold text-center mt-4;
+}
+</style>

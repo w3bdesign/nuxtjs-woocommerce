6f7572b6a274f166766552ff63f5fea391daedf0
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-04-05 21:04:30 +0200

    Npm refresh

--- components/Products/ProductsShowAll.vue
+++ components/Products/ProductsShowAll.vue
@@ -19,7 +19,7 @@
             >
               <img
                 id="product-image"
-                class="p-8 border mx-auto w-4/5 border-gray-200  rounded drop-shadow-lg transition duration-500 ease-in-out transform cursor-pointer lg:ml-0 lg:w-full lg:p-2 hover:scale-95"
+                class="p-8 border mx-auto w-4/5 border-gray-200 rounded drop-shadow-lg transition duration-500 ease-in-out transform cursor-pointer lg:ml-0 lg:w-full lg:p-2 hover:scale-95"
                 :alt="product.name"
                 :src="productImage(product)"
               />
@@ -71,7 +71,7 @@
             >
               <img
                 id="product-image"
-                class="p-8 border mx-auto w-4/5 border-gray-200  rounded drop-shadow-lg transition duration-500 ease-in-out transform cursor-pointer lg:ml-0 lg:w-full lg:p-2 hover:scale-95"
+                class="p-8 border mx-auto w-4/5 border-gray-200 rounded drop-shadow-lg transition duration-500 ease-in-out transform cursor-pointer lg:ml-0 lg:w-full lg:p-2 hover:scale-95"
                 :alt="product.name"
                 :src="productImage(product)"
               />

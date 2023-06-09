0251a02c207a27909bda8facd832d7c7a1a1e4ba
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-06-09 02:27:08 +0200

    Update LayoutNavItem.vue

--- components/Layout/LayoutNavItem.vue
+++ components/Layout/LayoutNavItem.vue
@@ -1,24 +1,24 @@
 <template>
-  <li
-    class="inline-block py-2 text-xl font-semibold no-underline lg:text-base lg:px-4"
-  >
-    <NuxtLink :to="to">
-      <span class="text-xl text-white no-underline lg:text-black">
-        {{ label }}
-      </span>
-    </NuxtLink>
-  </li>
+    <ul>
+        <li class="inline-block py-2 text-xl font-semibold no-underline lg:text-base lg:px-4">
+            <NuxtLink :to="to">
+                <span class="text-xl text-white no-underline lg:text-black">
+                    {{ label }}
+                </span>
+            </NuxtLink>
+        </li>
+    </ul>
 </template>
 
 <script setup>
 const props = defineProps({
-  to: {
-    type: String,
-    required: true,
-  },
-  label: {
-    type: String,
-    required: true,
-  },
+    to: {
+        type: String,
+        required: true,
+    },
+    label: {
+        type: String,
+        required: true,
+    },
 });
 </script>

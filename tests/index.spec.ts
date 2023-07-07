f4d26cfef92ad956e59a857151c638252598959d
codefactor-io <support@codefactor.io>
2023-07-07 14:34:06 +0000

    [CodeFactor] Apply fixes

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,8 +1,8 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('Has Index title', async ({ page }) => {
+test("Has Index title", async ({ page }) => {
   //await page.goto('https://playwright.dev/');
-  await page.goto('/');
+  await page.goto("/");
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Index - Nuxt 3 Woocommerce/);

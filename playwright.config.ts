8db77ee69fa21061776ec5bd92ee63cd4b491818
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-07-07 16:29:32 +0200

    Add Playwright test

--- playwright.config.ts
+++ playwright.config.ts
@@ -74,4 +74,17 @@ export default defineConfig({
   //   url: 'http://127.0.0.1:3000',
   //   reuseExistingServer: !process.env.CI,
   // },
+
+
+    /* Run your local dev server before starting the tests */
+    webServer: {
+      reuseExistingServer: true,
+      command: 'npm run dev',
+      port: 3000,
+    },
+
+
+
+
+
 });
---
+++ tests/index.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+test('Has Index title', async ({ page }) => {
+  //await page.goto('https://playwright.dev/');
+  await page.goto('/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Index - Nuxt 3 Woocommerce/);
+});

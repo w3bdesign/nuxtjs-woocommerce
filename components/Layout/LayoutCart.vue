5f449e34e116147388ae646a3229917b4ec047e2
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-07-09 21:41:01 +0200

    Update LayoutCart.vue

--- components/Layout/LayoutCart.vue
+++ components/Layout/LayoutCart.vue
@@ -1,48 +1,46 @@
 <template>
-  <div>
-    <div v-if="remoteError">
-      <span class="text-xl text-red-500"
-        >Error fetching cart. Please refresh the page.</span
-      >
-    </div>
-    <div v-else class="mt-4 lg:mt-0">
-      <NuxtLink to="/cart">
-        <transition name="cart">
-          <span
-            v-if="cartLength"
-            class="text-xl text-white no-underline lg:text-black is-active"
-          >
-            <span class="hidden lg:block">
-              <nuxt-img
-                alt="Cart icon"
-                class="h-12 ml-4 lg:ml-2"
-                aria-label="Cart"
-                src="/svg/Cart.svg"
-              />
-            </span>
-            <span class="block lg:hidden">
-              <Icon
-                name="ri:shopping-cart-2-line"
-                size="3em"
-                class="text-white lg:text-black"
-              />
-            </span>
+  <div v-if="remoteError">
+    <span class="text-xl text-red-500"
+      >Error fetching cart. Please refresh the page.</span
+    >
+  </div>
+  <div v-else class="mt-4 lg:mt-0">
+    <NuxtLink to="/cart">
+      <transition name="cart">
+        <span
+          v-if="cartLength"
+          class="text-xl text-white no-underline lg:text-black is-active"
+        >
+          <span class="hidden lg:block">
+            <nuxt-img
+              alt="Cart icon"
+              class="h-12 ml-4 lg:ml-2"
+              aria-label="Cart"
+              src="/svg/Cart.svg"
+            />
           </span>
-        </transition>
-        <transition name="cart">
-          <div v-if="cartLength">
-            <span
-              class="absolute w-6 h-6 pb-2 ml-16 -mt-12 text-center text-black bg-white lg:text-white lg:bg-black rounded-full lg:ml-14"
-            >
-              {{ cartLength }}
-            </span>
-            <span class="text-white lg:text-black">
-              Total: {{ formatPrice(`${subTotal}`) }}
-            </span>
-          </div>
-        </transition>
-      </NuxtLink>
-    </div>
+          <span class="block lg:hidden">
+            <Icon
+              name="ri:shopping-cart-2-line"
+              size="3em"
+              class="text-white lg:text-black"
+            />
+          </span>
+        </span>
+      </transition>
+      <transition name="cart">
+        <div v-if="cartLength">
+          <span
+            class="absolute w-6 h-6 pb-2 ml-16 -mt-12 text-center text-black bg-white lg:text-white lg:bg-black rounded-full lg:ml-14"
+          >
+            {{ cartLength }}
+          </span>
+          <span class="text-white lg:text-black">
+            Total: {{ formatPrice(`${subTotal}`) }}
+          </span>
+        </div>
+      </transition>
+    </NuxtLink>
   </div>
 </template>
 

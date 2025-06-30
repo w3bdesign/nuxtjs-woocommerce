4f9a0bac8f54bec51b238f58e78718f56fd9ac54
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2025-06-30 02:41:46 +0200

    Create ColorSwatch.vue

---
+++ components/common/ColorSwatch.vue
@@ -0,0 +1,20 @@
+<template>
+  <span
+    class="inline-block w-8 h-8 rounded-full border-2 border-white shadow"
+    :style="{ backgroundColor: color }"
+    :title="title"
+  ></span>
+</template>
+
+<script setup>
+defineProps({
+  color: {
+    type: String,
+    default: '#3b82f6', // Tailwind blue-500
+  },
+  title: {
+    type: String,
+    default: '',
+  },
+});
+</script>

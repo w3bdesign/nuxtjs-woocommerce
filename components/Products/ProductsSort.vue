2027ad7ccf03678fbdf3670f4ef851b5cb144a4a
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2025-06-30 03:26:32 +0200

    Update ProductsSort.vue

--- components/Products/ProductsSort.vue
+++ components/Products/ProductsSort.vue
@@ -4,7 +4,6 @@
     <select
       id="sort-select"
       class="min-w-[140px] border border-gray-200 rounded-lg px-3 py-2 text-base bg-gray-50 text-gray-700 focus:outline-none"
-      disabled
     >
       <option value="popular">Populær</option>
       <option value="price-low">Pris: Lav til Høy</option>

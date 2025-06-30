31575adf8f17d0322e555843a195b2b80433bd1b
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2025-06-30 03:25:44 +0200

    Update RangeSlider.vue

--- components/common/RangeSlider.vue
+++ components/common/RangeSlider.vue
@@ -4,8 +4,7 @@
       type="range"
       min="0"
       max="1000"
-      class="w-full accent-blue-600"
-      disabled
+      class="w-full accent-blue-600"     
     />
     <div class="flex justify-between text-sm mt-1">
       <span>kr 0</span>

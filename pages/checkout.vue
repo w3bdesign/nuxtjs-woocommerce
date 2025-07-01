7e74365d23a8e1898b1a1f7dc62b928c20099da4
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2025-07-01 04:00:11 +0200

    Fix checkout

--- pages/checkout.vue
+++ pages/checkout.vue
@@ -6,21 +6,6 @@
 </template>
 
 <script setup>
-import { useCart } from "@/store/useCart";
-import { watch } from "vue";
-
-const cart = useCart();
-
-watch(
-  () => cart.loading,
-  (loading) => {
-    if (!loading && cart.cart.length === 0) {
-      navigateTo("/");
-    }
-  },
-  { immediate: true },
-);
-
 useHead({
   title: "Checkout",
   titleTemplate: "%s - Nuxt 3 Woocommerce",
